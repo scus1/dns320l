69d8d382d798061274e45d28abeb49418ca3bc9b
Sunke Schlüters <sunke@schlueters.net>
2015-06-04 15:49:21 +0200

    Fixed path to kirkwood-dns320l.dtb

--- kernel/build.sh
+++ kernel/build.sh
@@ -5,7 +5,7 @@ RELEASE=$(cross-make kernelrelease)
 echo "Building $RELEASE"
 
 cross-make -j8 zImage kirkwood-dns320l.dtb || exit 1
-cat arch/arm/boot/kirkwood-dns320l.dtb >> arch/arm/boot/zImage || exit 1
+cat arch/arm/boot/dts/kirkwood-dns320l.dtb >> arch/arm/boot/zImage || exit 1
 
 cross-make uImage || exit 1
 cross-make -j8 modules || exit 1
