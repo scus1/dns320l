b5a150146f73c64f2721c99810c8965bb675de54
Sunke Schlüters <sunke@schlueters.net>
2015-06-08 22:24:05 +0200

    Added daemon to control MCU

--- kernel/build.sh
+++ kernel/build.sh
@@ -4,8 +4,14 @@ alias cross-make='make ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-'
 RELEASE=$(cross-make kernelrelease)
 echo "Building $RELEASE"
 
+echo "Building kernel image"
 cross-make -j8 zImage kirkwood-dns320l.dtb || exit 1
+
+echo "Appending DTB"
 cat arch/arm/boot/dts/kirkwood-dns320l.dtb >> arch/arm/boot/zImage || exit 1
 
+echo "Building uImage"
 cross-make LOADADDR=0x00008000 uImage || exit 1
+
+echo "Building modules"
 cross-make -j8 modules || exit 1
